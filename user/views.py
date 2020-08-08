29c8b1f92c967217ea67aa686016282d38ea222e
hruday-tej <hruday.tej.akk@gmail.com>
2020-08-09 04:50:22 +0530

    done with graphs

--- user/views.py
+++ user/views.py
@@ -73,19 +73,90 @@ def admin_ques_detail(request,ques_pk):
 	female = 0
 	other = 0
 
-	# male_yes = 0
-	# male_no = 0
-	# female_yes = 0
-	# female_no = 0
+	male_yes = 0
+	male_no = 0
+	female_yes = 0
+	female_no = 0
+	other_yes = 0
+	other_no=0
+
+	male_neutral = 0
+	female_neutral = 0
+	other_neutral = 0
+
+
+	yes=0
+	no=0
+	neutral=0
 	# male_neutral = 0 
-	answers = Answer.objects.filter(question = question)
-	for i in answers:
-		if i.gender == 'male':
-			male +=1
-		elif i.gender == 'female':
-			female +=1
-		else :
-			other +=1
+	responses = Answer.objects.filter(question = question)
+	for i in responses:
+		if i.answer == 'No':
+			no+=1
+			if i.gender == 'male':
+				male_no +=1
+			elif i.gender == 'female':
+				female_no +=1
+			else:
+				other_no += 1
+		elif i.answer == 'Neutral':
+			neutral +=1
+			if i.gender == 'male':
+				male_neutral +=1
+			elif i.gender == 'female':
+				female_neutral +=1
+			else:
+				other_neutral += 1
+
+		else:
+			yes+=1
+			if i.gender == 'male':
+				male_yes +=1
+			elif i.gender == 'female':
+				female_yes +=1
+			else:
+				other_yes +=1
+
+	labels=[]
+	data=[]
+	labelsyes=[]
+	datayes=[]
+	labelsno=[]
+	datano=[]
+	labelsneutral=[]
+	dataneutral=[]
+
+	labelsyes.append('Male')
+	labelsyes.append('Female')
+	labelsyes.append('Other')
+
+	datayes.append(male_yes)
+	datayes.append(female_yes)
+	datayes.append(other_yes)
+
+	labelsno.append('Male')
+	labelsno.append('Female')
+	labelsno.append('Other')
+
+	datano.append(male_no)
+	datano.append(female_no)
+	datano.append(other_no)
+
+	labelsneutral.append('Male')
+	labelsneutral.append('Female')
+	labelsneutral.append('Other')
+
+	dataneutral.append(male_neutral)
+	dataneutral.append(female_neutral)
+	dataneutral.append(other_neutral)
 
 
-	return render(request,'user/admin-ques-detail.html',{'male':male,'female':female,'other':other})
+	labels.append('Satisfied')
+	labels.append('Not Satisfied')
+	labels.append('Neutral')
+	data.append(yes)
+	data.append(no)
+	data.append(neutral)
+
+
+	return render(request,'user/admin-ques-detail.html',{'labels':labels,'data':data, 'labelsneutral':labelsneutral,'dataneutral':dataneutral,'labelsyes':labelsyes,'datayes':datayes,'labelsno':labelsno,'datano':datano,'question':question})
